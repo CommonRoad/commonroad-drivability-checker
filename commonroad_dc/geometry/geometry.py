f281493c9c18d3bf908372e2bc42ae73413a2178
Daniel Tar <tardani95@gmail.com>
2021-09-01 16:02:01 +0200

    _set_curvature method

--- commonroad_dc/geometry/geometry.py
+++ commonroad_dc/geometry/geometry.py
@@ -1,5 +1,8 @@
 import commonroad_dc.pycrccosy as pycrccosy
-from commonroad_dc.geometry.util import chaikins_corner_cutting, resample_polyline
+import numpy as np
+
+from commonroad_dc.geometry.util import chaikins_corner_cutting, resample_polyline, \
+    compute_curvature_from_polyline
 
 
 class RefPathLengthException(Exception):
@@ -17,3 +20,14 @@ class CurvilinearCoordinateSystem(pycrccosy.CurvilinearCoordinateSystem):
             raise RefPathLengthException("Reference path length is invalid")
 
         super().__init__(ref_path, default_projection_domain_limit, eps, eps2)
+
+    def _set_curvature(self, ccosy_ref_path_curvature: np.ndarray):
+
+        # commonroad_dc.pycrccosy extends the original polyline with 3 segments(vertex in the beginning)
+        # and with 2 segments(at the end) so we have to adjust the curvatre to this
+        modified_curvature = [0.0] * 3 + list(ccosy_ref_path_curvature) + [0.0] * 2
+        self.set_curvature(modified_curvature)
+
+    def calc_and_set_curvature(self):
+        curvature = compute_curvature_from_polyline(np.array(self.reference_path()))
+        self._set_curvature(curvature)
