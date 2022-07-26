fa18f23d8c4af73b06dba9bc8067a0e0b1e259bf
Vitalii Rusinov <ge56cug@mytum.de>
2022-07-26 12:56:19 +0200

    Update ci/clang-tidy.sh

--- ci/clang-tidy.sh
+++ ci/clang-tidy.sh
@@ -8,10 +8,10 @@ exit_flag=false
 clang_tidy_version='10'
 clang_tidy="clang-tidy-$clang_tidy_version"
 
-target_branch="origin master"
+target_branch="master"
 
 # Retrieve list of cpp-files that were changed in source branch with respect to master (target branch)
-filelist=($(git diff ${target_branch}... --name-only | grep ".cpp"))
+filelist=($(git diff origin/${target_branch} --name-only | grep ".cpp"))
 
 
 if [[ "${#filelist[@]}" -eq "0" ]]; then
