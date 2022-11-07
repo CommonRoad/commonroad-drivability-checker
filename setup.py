07952cf14d272c5a09a0cc101ffa1b65a7bdc381
Gerald Wuersching <gerald.wuersching@tum.de>
2022-11-07 16:24:52 +0100

    update requirements

--- setup.py
+++ setup.py
@@ -202,7 +202,7 @@ setup(
     python_requires='>=3.6',
     install_requires=[
         'commonroad-io>=2022.2',
-        'commonroad-vehicle-models>=1.0.0',
+        'commonroad-vehicle-models>=3.0.0',
         'numpy>=1.19',
         'scipy>=1.4.1',
         'matplotlib>=3.2.2',
