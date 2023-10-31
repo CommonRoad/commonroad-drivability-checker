48d1140220313adc9697a8cb2e1139945c33ad88
Tobias Markus <tobias@miglix.eu>
2023-10-31 15:08:09 +0100

    Use FetchContent_Declare_Fallback for pybind11
    Other projects that include the Drivability Checker might want to
    override the used pybind11 package. FetchContent_Declare_Fallback (from
    commonroad-cmake) allows using the FIND_PACKAGE_ARGS feature introduced
    in CMake v3.24 if it is available, without causing errors on earlier
    CMake versions.

--- third_party/external_pybind11.cmake
+++ third_party/external_pybind11.cmake
@@ -1,16 +1,19 @@
 include_guard(DIRECTORY)
 
 include(FetchContent)
+include(utils/FetchContentHelper)
 
 if(POLICY CMP0135)
   cmake_policy(SET CMP0135 NEW)
 endif()
 
-FetchContent_Declare(
+FetchContent_Declare_Fallback(
   pybind11
 
   URL            https://github.com/pybind/pybind11/archive/refs/tags/v2.11.1.tar.gz
   URL_HASH       SHA256=d475978da0cdc2d43b73f30910786759d593a9d8ee05b1b6846d1eb16c6d2e0c
+
+  FIND_PACKAGE_ARGS
   )
 
 # set(PYBIND11_CPP_STANDARD -std=c++17)
