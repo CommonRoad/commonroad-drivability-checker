28e877ffe41feab2d7442bcbe97084f23507eb7b
Gerald Wuersching <gerald.wuersching@tum.de>
2024-12-03 18:44:08 +0100

    minor fix classifiers

--- setup.py
+++ setup.py
@@ -217,8 +217,6 @@ setup(
     # Additional information
     classifiers=[
         "Programming Language :: C++",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
