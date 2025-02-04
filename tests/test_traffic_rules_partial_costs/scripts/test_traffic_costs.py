7d9af9571cb8a90eb9732526597a76bac0845c6c
ugo <ugo.onuoha@tum.de>
2025-02-04 15:13:57 +0100

    check tests

--- tests/test_traffic_rules_partial_costs/scripts/test_traffic_costs.py
+++ tests/test_traffic_rules_partial_costs/scripts/test_traffic_costs.py
@@ -847,5 +847,10 @@ class TestStopAtTrafficLightCost(unittest.TestCase):
     
 
        
-if __name__ == '__main__':
-    unittest.main()       
+# if __name__ == '__main__':
+#     unittest.main()       
+
+if __name__ == "__main__":
+    import sys
+    if "unittest" not in sys.modules:
+        unittest.main()
