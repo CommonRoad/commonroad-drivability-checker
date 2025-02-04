58e26909697aa06e31438ea863c328d3c997d006
ugo <ugo.onuoha@tum.de>
2025-02-04 14:27:31 +0100

    check tests

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -21,18 +21,21 @@ if __name__ == "__main__":
     #         tests += b.testsRun
     
     for x in os.walk(os.getcwd()):
-        # Allow 'test_traffic_rules_partial_costs' but exclude other 'costs' folders
+        # TODO costs tests need to be fixed and are temporarily disabled in CI
+        # TODO some feasibility tests have a scipy-related bug and is temporarily disabled in CI
+        # Keep original filtering, but allow 'test_traffic_rules_partial_costs/scripts'
         if not '__' in x[0] and not '.' in x[0] and not 'feasibility' in x[0]:
-            if 'costs' in x[0] and 'test_traffic_rules_partial_costs' not in x[0]:
+            if 'costs' in x[0] and 'test_traffic_rules_partial_costs/scripts' not in x[0]:
                 continue  # Skip other 'costs' directories, but allow this one
-
             print(x[0])
             all_tests = unittest.TestLoader().discover(x[0], pattern='test_*.py')
             b = unittest.TextTestRunner().run(all_tests)
             failures += len(b.failures)
             errors += len(b.errors)
             tests += b.testsRun
-    
+            
+            
+
     print(
         'Executed {} tests; got {} fails and {} errors'.format(tests, failures, errors)
     )
