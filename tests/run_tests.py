87b4bb51a51a80078a79a219639cbaa91a0fa11f
ugo <ugo.onuoha@tum.de>
2025-02-04 19:59:54 +0100

    check tests

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -2,10 +2,6 @@ import unittest as unittest
 import os
 import sys
 
-crmonitor_path = "/home/ugochukwu/miniconda3/envs/commonroad-py39/lib/python3.9/site-packages"
-if crmonitor_path not in sys.path:
-    sys.path.insert(0, crmonitor_path)
-    
 if __name__ == "__main__":
     print(os.getcwd())
     errors = 0
