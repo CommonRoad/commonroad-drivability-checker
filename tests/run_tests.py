b9242bb66a3b1152ec9eb61dfd86967037081121
ugo <ugo.onuoha@tum.de>
2025-02-04 14:07:08 +0100

    check tests

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -9,26 +9,29 @@ if __name__ == "__main__":
     errors = 0
     failures = 0
     tests = 0
+    # for x in os.walk(os.getcwd()):
+    #     # TODO costs tests need to be fixed and are temporarily disabled in CI
+    #     # TODO some feasibility tests have a scipy-related bug and is temporarily disabled in CI
+    #     if not '__' in x[0] and not '.' in x[0] and not 'costs' in x[0] and not 'feasibility' in x[0]:
+    #         print(x[0])
+    #         all_tests = unittest.TestLoader().discover(x[0], pattern='test_*.py')
+    #         b = unittest.TextTestRunner().run(all_tests)
+    #         failures += len(b.failures)
+    #         errors += len(b.errors)
+    #         tests += b.testsRun
+    
     for x in os.walk(os.getcwd()):
-        # TODO costs tests need to be fixed and are temporarily disabled in CI
-        # TODO some feasibility tests have a scipy-related bug and is temporarily disabled in CI
-        if not '__' in x[0] and not '.' in x[0] and not 'costs' in x[0] and not 'feasibility' in x[0]:
+        # Allow 'test_traffic_rules_partial_costs' but exclude other 'costs' folders
+        if not '__' in x[0] and not '.' in x[0] and not 'feasibility' in x[0]:
+            if 'costs' in x[0] and 'test_traffic_rules_partial_costs' not in x[0]:
+                continue  # Skip other 'costs' directories, but allow this one
+
             print(x[0])
             all_tests = unittest.TestLoader().discover(x[0], pattern='test_*.py')
             b = unittest.TextTestRunner().run(all_tests)
             failures += len(b.failures)
             errors += len(b.errors)
             tests += b.testsRun
-            
-    # Run test_traffic_costs.py
-    specific_test = Path(os.getcwd()) / "test_traffic_rules_partial_costs/scripts/test_traffic_costs.py"
-    assert specific_test.exists(), f"Error: Test file {specific_test} does not exist!"
-    print(f"Running traffic rule specific partial cost function test: {specific_test}")
-    all_tests = unittest.TestLoader().discover(os.path.dirname(specific_test), pattern="test_traffic_costs.py")
-    b = unittest.TextTestRunner().run(all_tests)
-    failures += len(b.failures)
-    errors += len(b.errors)
-    tests += b.testsRun
     
     print(
         'Executed {} tests; got {} fails and {} errors'.format(tests, failures, errors)
