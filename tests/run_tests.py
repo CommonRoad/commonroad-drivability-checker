7f6e97d0cf792fa1b30b43fc47304a2242014d49
ugo <ugo.onuoha@tum.de>
2025-02-04 14:58:15 +0100

    check tests

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -9,24 +9,10 @@ if __name__ == "__main__":
     errors = 0
     failures = 0
     tests = 0
-    # for x in os.walk(os.getcwd()):
-    #     # TODO costs tests need to be fixed and are temporarily disabled in CI
-    #     # TODO some feasibility tests have a scipy-related bug and is temporarily disabled in CI
-    #     if not '__' in x[0] and not '.' in x[0] and not 'costs' in x[0] and not 'feasibility' in x[0]:
-    #         print(x[0])
-    #         all_tests = unittest.TestLoader().discover(x[0], pattern='test_*.py')
-    #         b = unittest.TextTestRunner().run(all_tests)
-    #         failures += len(b.failures)
-    #         errors += len(b.errors)
-    #         tests += b.testsRun
-    
     for x in os.walk(os.getcwd()):
         # TODO costs tests need to be fixed and are temporarily disabled in CI
         # TODO some feasibility tests have a scipy-related bug and is temporarily disabled in CI
-        # Keep original filtering, but allow 'test_traffic_rules_partial_costs/scripts'
-        if not '__' in x[0] and not '.' in x[0] and not 'feasibility' in x[0]:
-            if 'costs' in x[0] and 'test_traffic_rules_partial_costs/scripts' not in x[0]:
-                continue  # Skip other 'costs' directories, but allow this one
+        if not '__' in x[0] and not '.' in x[0] and not 'costs' in x[0] and not 'feasibility' in x[0]:
             print(x[0])
             all_tests = unittest.TestLoader().discover(x[0], pattern='test_*.py')
             b = unittest.TextTestRunner().run(all_tests)
@@ -34,7 +20,16 @@ if __name__ == "__main__":
             errors += len(b.errors)
             tests += b.testsRun
             
-            
+    # Run the traffic rule partial costs tests
+    test_script_path = os.path.join(os.getcwd(), "test_traffic_rules_partial_costs/scripts/test_traffic_costs.py")
+    assert os.path.exists(test_script_path), f"Error: Test script {test_script_path} not found!"
+    print(f"Now running {test_script_path}")
+    suite = unittest.defaultTestLoader.loadTestsFromName("test_traffic_rules_partial_costs.scripts.test_traffic_costs")
+    result = unittest.TextTestRunner().run(suite)
+    failures += len(result.failures)
+    errors += len(result.errors)
+    tests += result.testsRun
+
 
     print(
         'Executed {} tests; got {} fails and {} errors'.format(tests, failures, errors)
