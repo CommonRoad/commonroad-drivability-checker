9cd3bb4c6aaea87c29d218537aa638594c7d5196
ugo <ugo.onuoha@tum.de>
2025-02-05 07:50:48 +0100

    check tests

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     for x in os.walk(os.getcwd()):
         # TODO costs tests need to be fixed and are temporarily disabled in CI
         # TODO some feasibility tests have a scipy-related bug and is temporarily disabled in CI
-        if not '__' in x[0] and not '.' in x[0] and not 'costs' in x[0] and not 'feasibility' in x[0]:
+        if not '__' in x[0] and not '.' in x[0] and not 'costs' in x[0] and not 'feasibility' in x[0] and not 'traffic_rules_costs' in x[0]:
             print(x[0])
             all_tests = unittest.TestLoader().discover(x[0], pattern='test_*.py')
             b = unittest.TextTestRunner().run(all_tests)
